20417b132d95df3fec8369d8d16563678b19cc7e
Iliya Iliev <iliya.ilievbg@gmail.com>
2023-02-28 12:29:46 +0200

    Adding Problems
    Adding Problems

---
+++ Objects and Classes/Objects and Classes - More Exercise/1. Company Roster/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Company_Roster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<Employee>> departments = new Dictionary<string, List<Employee>>();
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                string[] currentEmployeeInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string department = GetDepartment(currentEmployeeInfo[2]);
+
+                Employee currentEmployee = new Employee();
+                currentEmployee.Name = currentEmployeeInfo[0];
+                currentEmployee.Salary = decimal.Parse(currentEmployeeInfo[1]);
+                currentEmployee.Department = department;
+
+                if (!departments.ContainsKey(department))
+                {
+                    departments[department] = new List<Employee>();
+                    departments[department].Add(currentEmployee);
+                }
+                else
+                {
+                    departments[department].Add(currentEmployee);
+                }
+            }
+            var orderedDepartments = departments.OrderByDescending(x => x.Value.Average(x => x.Salary)).ToList();
+            Console.WriteLine($"Highest Average Salary: {orderedDepartments[0].Key}");
+          
+            List<Employee> employeesBySalary = orderedDepartments[0].Value.OrderByDescending(e => e.Salary).ToList();
+            foreach (Employee employee in employeesBySalary)
+            {
+                Console.Write($"{employee.Name} ");
+                Console.WriteLine($"{employee.Salary:F2}");
+            }
+        }
+
+        private static string GetDepartment(string departmentName)
+        {
+            return departmentName;
+        }
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ Objects and Classes/Objects and Classes - More Exercise/2.OldestFamilyMember/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.OldestFamilyMember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < peopleCount; i++)
+            {
+                string[] currentPerson = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Person currentFamilyMember = new Person(currentPerson[0], int.Parse(currentPerson[1]));
+                
+                family.AddMember(currentFamilyMember);
+            }
+            var oldestFamilyMember = family.GetOldestMember(family.FamilyMembers);
+            Console.Write($"{oldestFamilyMember.Name} ");
+            Console.WriteLine(oldestFamilyMember.Age);
+        }
+
+    }
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class Family
+    {
+        public Family()
+        {
+            this.FamilyMembers = new List<Person>();
+        }
+        public List<Person> FamilyMembers { get; set; }
+        
+        public void AddMember(Person name)
+        {
+            FamilyMembers.Add(name);
+        }
+        public Person GetOldestMember(List<Person> familyMembers)
+        {
+            List<Person> orderedFamilyMembers = familyMembers.OrderByDescending(x => x.Age).ToList();
+            return orderedFamilyMembers[0];
+        }
+    }
+}
