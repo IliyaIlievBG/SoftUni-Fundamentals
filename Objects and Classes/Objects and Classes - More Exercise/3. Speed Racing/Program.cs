6134baa4542891f2c089a9d411d8ba6481a30928
Iliya Iliev <iliya.ilievbg@gmail.com>
2023-03-04 22:07:28 +0200

    adding problems
    adding problems

---
+++ Objects and Classes/Objects and Classes - More Exercise/3. Speed Racing/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Speed_Racing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsToTrack = int.Parse(Console.ReadLine());
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+
+            for (int i = 0; i < carsToTrack; i++)
+            {
+                string[] currentCarInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = GetModel(currentCarInfo);
+                double fuelAmount = GetFuel(currentCarInfo);
+                double consumptionPerKm = GetConsumption(currentCarInfo);
+                
+                if (!cars.ContainsKey(currentCarInfo[0]))
+                {
+                    //if car is not in the dictionary, creat one and add it. Else don't do nothing
+                    Car nextCar = new Car(model, fuelAmount, consumptionPerKm);
+                    nextCar.TravelDistance = 0;
+                    cars.Add(model, nextCar);
+                }    
+            }
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                //implement logic
+                string[] currentCommand = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = currentCommand[1];
+                double distance = double.Parse(currentCommand[2]);
+                if (cars.ContainsKey(model))
+                {
+                    cars[model].Drive(distance);
+                }
+
+                command = Console.ReadLine();
+            }
+            foreach (KeyValuePair<string, Car> car in cars)
+            {
+                Console.WriteLine($"{car.Value.Model} {car.Value.FuelAmount:F2} {car.Value.TravelDistance}");
+            }
+        }
+
+        private static double GetConsumption(string[] currentCarInfo)
+        {
+            return double.Parse(currentCarInfo[2]);
+        }
+
+        private static double GetFuel(string[] currentCarInfo)
+        {
+            return double.Parse(currentCarInfo[1]);
+        }
+
+        private static string GetModel(string[] currentCarInfo)
+        {
+            return currentCarInfo[0];
+        }
+    }
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double consumptionPerKm)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            ConsumptionPerKm = consumptionPerKm;
+        }
+
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double ConsumptionPerKm { get; set; }
+
+        public double TravelDistance { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (distance*ConsumptionPerKm>FuelAmount)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                TravelDistance += distance;
+                FuelAmount -= ConsumptionPerKm * distance;
+            }
+        }
+    }
+}
---
+++ Objects and Classes/Objects and Classes - More Exercise/4. Raw Data/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Raw_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsCount = int.Parse(Console.ReadLine());
+            List<Car> allCars = new List<Car>();
+
+            for (int i = 0; i < carsCount; i++)
+            {
+                string[] currentCarInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string model = GetCarInfo(currentCarInfo, 0);
+                int engineSpeed = GetNumericCarInfo(currentCarInfo, 1);
+                int enginePower = GetNumericCarInfo(currentCarInfo, 2);
+                int cargoWeight = GetNumericCarInfo(currentCarInfo, 3);
+                string cargoType = GetCarInfo(currentCarInfo, 4);
+                
+                Engine currentEngine = new Engine();
+                currentEngine.Speed = engineSpeed;
+                currentEngine.Power = enginePower;
+
+                Cargo currentCargo = new Cargo();
+                currentCargo.Weight = cargoWeight;
+                currentCargo.Type = cargoType;
+
+                Car currentCar = new Car(model, currentEngine, currentCargo);
+                allCars.Add(currentCar);
+            }
+            var carsWithFlamableCargo = allCars.Where(x => x.Cargo.Type == "flamable").ToList();
+            var carsWithFragileCargo = allCars.Where(x => x.Cargo.Type == "fragile").ToList();
+
+            string command = Console.ReadLine();
+            switch (command)
+            {
+                case "fragile":
+                    foreach (var item in carsWithFragileCargo)
+                    {
+                        if (item.Cargo.Weight < 1000)
+                        {
+                            Console.WriteLine(item.Model);
+                        }
+                    }
+                    break;
+                case "flamable":
+                    
+                    break;
+            }
+
+        }
+
+        private static string GetCarInfo(string[] currentCarInfo, int index)
+        {
+            return currentCarInfo[index];
+        }
+        private static int GetNumericCarInfo(string[] currentCarInfo, int index)
+        {
+            return int.Parse(currentCarInfo[index]);
+        }
+    }
+    public class Car
+    {
+        public Car(string model, Engine engine, Cargo cargo)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+        }
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+    }
+    public class Engine
+    {
+        public int Speed { get; set; }
+        public int Power { get; set; }
+    }
+    public class Cargo
+    {
+        public int Weight { get; set; }
+        public string Type { get; set; }
+    }
+}
