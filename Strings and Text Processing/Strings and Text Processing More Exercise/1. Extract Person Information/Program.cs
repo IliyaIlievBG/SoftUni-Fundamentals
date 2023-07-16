026165ad1f54f5e0e58a657c90b053a2a4ae5273
Iliya Iliev <iliya.ilievbg@gmail.com>
2023-07-16 14:10:09 +0300

    adding task

---
+++ Strings and Text Processing/Strings and Text Processing More Exercise/1. Extract Person Information/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string currentPersonInfo = Console.ReadLine();
+                string name = GetPersonName(currentPersonInfo);
+                
+            }
+        }
+
+        private static string GetPersonName(string currentPersonInfo)
+        {
+            int startIndex = currentPersonInfo.IndexOf('@');
+            int endIndex = currentPersonInfo.IndexOf('|');
+
+            string name = currentPersonInfo.Substring(startIndex+1, endIndex-startIndex-1);
+            return name;
+        }
+    }
+}
