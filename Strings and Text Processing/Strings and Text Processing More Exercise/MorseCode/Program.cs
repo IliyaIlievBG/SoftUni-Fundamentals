f022c779307b71539010d1369e1fb1530301595b
Iliya Iliev <iliya.ilievbg@gmail.com>
2023-08-07 02:14:13 +0300

    Adding tasks

---
+++ Strings and Text Processing/Strings and Text Processing More Exercise/MorseCode/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MorseCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] encryptedMessage = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Dictionary<char, string> morseCode = DecryptMorseCode();
+
+            foreach (var letter in encryptedMessage)
+            {
+                char nextLetter = GetLetter(morseCode, letter);
+                Console.Write(nextLetter);
+            }
+            Console.WriteLine();
+        }
+
+        private static char GetLetter(Dictionary<char, string> morseCode, string codedLetter)
+        {
+            char nextLetter = '\0';
+            foreach (var letter in morseCode)
+            {
+                if (letter.Value == codedLetter) 
+                {
+                    nextLetter = letter.Key;
+                }
+            }
+            return char.ToUpper(nextLetter);
+        }
+
+        private static Dictionary<char, string> DecryptMorseCode()
+        {
+            return new Dictionary<char, string>()
+            {
+                {'a', ".-"},
+                {'b', "-..."},
+                {'c', "-.-."},
+                {'d', "-.."},
+                {'e', "."},
+                {'f', "..-."},
+                {'g', "--."},
+                {'h', "...."},
+                {'i', ".."},
+                {'j', ".---"},
+                {'k', "-.-"},
+                {'l', ".-.."},
+                {'m', "--"},
+                {'n', "-."},
+                {'o', "---"},
+                {'p', ".--."},
+                {'q', "--.-"},
+                {'r', ".-."},
+                {'s', "..."},
+                {'t', "-"},
+                {'u', "..-"},
+                {'v', "...-"},
+                {'w', ".--"},
+                {'x', "-..-"},
+                {'y', "-.--"},
+                {'z', "--.."},
+                {' ', "|" }
+            };
+        }
+    }
+}
---
+++ Strings and Text Processing/Strings and Text Processing More Exercise/TreasueFind/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TreasureFind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] key = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            string treasureInfo = Console.ReadLine();
+            while (treasureInfo != "find")
+            {
+                StringBuilder decryptor = new StringBuilder();
+
+                for (int i = 0; i < treasureInfo.Length; i++)
+                {
+                    for (int j = 0; j < key.Length; j++)
+                    {
+                        if (i == treasureInfo.Length)
+                        {
+                            break;
+                        }
+                        int currentLetterToInt = treasureInfo[i] - key[j];
+                        i++;
+                        char currentDecryptedLetter = (char)currentLetterToInt;
+
+                        decryptor.Append(currentDecryptedLetter);
+                    }
+                    i--;
+                }
+
+                var decryptedMessage = decryptor.ToString();
+                string treasureType = GetTreasureDetails(decryptedMessage, '&', '&');
+                var coordinates = GetTreasureDetails(decryptedMessage, '<', '>');
+
+                Console.WriteLine($"Found {treasureType} at {coordinates}");
+                treasureInfo = Console.ReadLine();
+            }
+        }
+
+        private static string GetTreasureDetails(string decryptedMessage, char start, char end)
+        {
+            int startIndex = decryptedMessage.IndexOf(start);
+            if (startIndex != -1)
+            {
+                int endIndex = decryptedMessage.LastIndexOf(end);
+                if (endIndex != -1)
+                {
+                    return decryptedMessage.Substring(startIndex + 1, endIndex - startIndex - 1);
+                }
+            }
+            return String.Empty;
+        }
+    }
+}
